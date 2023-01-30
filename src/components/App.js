629834cd16e75b923a36199eae6ae630ddd7810d
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-01-30 12:11:54 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import '../styles/App.scss'
+import Header from "./Header"
+/* 
+create an instagram for animals
+have a nav bar labeled "good bois" "danger noodles" "nopes"
+create json for each category
+using map make a function that creates cards for each object with a photo, name, and description
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Content */}
+    </div>
+  );
+}
+
+export default App;
