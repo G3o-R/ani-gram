ce33a06bde6a15363f522b9f8366716b7b6c1cd4
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-02-01 20:07:20 -0700

    mapped data too page

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import "../styles/Card.scss"
+
+function Card({name,image,description,family,id}){
+    return(
+    <div className="card">
+        <div className="image-container">
+            <img src={image} alt={name} className="image" />
+        </div>
+        <div className="body-container">
+            <p className="name">{name}</p>
+            <p className="description">{description}</p>
+        </div>
+        <div className="bottom">{family}</div>
+    </div>
+    )
+}
+
+export default Card
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,20 @@
+import "../styles/Home.scss"
+import Card from "./Card"
+
 function Home({animalData}){
     console.log(animalData)
     return(
-        <div>
-
+        <div className="content">
+            {/* test */}
+            {animalData.map((animal)=>(
+                <Card 
+                name={animal.name}
+                image={animal.image}
+                description={animal.description}
+                family={animal.family}
+                id={animal.id}
+                />
+            ))}
         </div>
     )
 }
