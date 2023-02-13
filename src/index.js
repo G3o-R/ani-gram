41abd6f523637f97cb94dbda633a7d0a4e6e0bcb
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-02-13 15:40:58 -0700

    fixed index for react@18.2.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import './styles/index.scss';
 import App from './components/App';
 
-ReactDOM.render(
-  <Router>
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
     <App />
-  </Router>,
-  document.getElementById("root")
+    </Router>
 );
