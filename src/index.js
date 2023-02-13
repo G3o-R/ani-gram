9e5fc38a51f57f4aed449396b89f94b9dcab48dd
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-02-13 15:56:19 -0700

    removed comments from index

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './styles/index.scss';
 import App from './components/App';
 
-// ReactDOM.render(
-//   <Router>
-//     <App />
-//   </Router>,
-//   document.getElementById("root")
-// );
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
