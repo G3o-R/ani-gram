869c1a6bad58841fcc577d64194c32254a044c42
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-02-13 15:35:55 -0700

    removed a console.log

--- src/components/DangerNoodles.js
+++ src/components/DangerNoodles.js
@@ -3,7 +3,6 @@ import Card from "./Card"
 import GreaterCard from "./GreaterCard"
 
 function DangerNoodles({animalData}){
-    console.log("test")
     return(<div className="content-noodle">
         <div className="grid">
           {animalData.map((animal)=> <GreaterCard animal={animal} key={animal.id}/>)}
