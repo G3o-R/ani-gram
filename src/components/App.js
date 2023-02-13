c98f844aa1654a829139ed192f66424e16ffe538
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-02-13 16:00:49 -0700

    added an isVisible prop to NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* <Home animalData = {animals}/> */}
       <Switch>
         <Route exact path="/">
           <Home animalData={animals} setAnimals={setAnimals}/>
